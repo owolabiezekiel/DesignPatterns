658c2c8183152b278da5f2711cfe80a59a96e93f
Owolabi Tobiloba <owo.ezekiel@gmail.com>
2021-09-11 18:30:00 +0100

    Beginning Structural design patterns starting with Adapter pattern

---
+++ src/main/java/structural/adapter/TestAdapter.java
@@ -0,0 +1,16 @@
+package structural.adapter;
+
+/**
+ * Created by tobilobaowolabi on 11/09/2021
+ * Author: tobilobaowolabi
+ * Date: 11/09/2021
+ * Project: DesignPatterns
+ * IDE: IntelliJ IDEA
+ **/
+
+/**
+ * https://refactoring.guru/design-patterns/adapter
+ * https://www.tutorialspoint.com/design_pattern/adapter_pattern.htm
+ */
+public class TestAdapter {
+}
